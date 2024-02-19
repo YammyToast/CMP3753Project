e0166af2f0d976d4d2ea252177d3b4995c137080
YammyToast <cyanjamesmail@gmail.com>
2024-02-19 16:51:44 +0000

    Merging Changes

--- src/copland_testing/copland_testing/copland_main.py
+++ src/copland_testing/copland_testing/copland_main.py
@@ -7,17 +7,14 @@ import time
 
 from rclpy.node import Node
 from coplandnavi_interfaces.srv import SetupArgs
-from coplandnavi_interfaces.msg import NewSim
+from coplandnavi_interfaces.msg import NewSim, EndSim
 import rclpy
 
-class SimResult(Enum):
-    END = 0
-    NOTREADY = 1
 
 
 
 REQUIRED_NODES = ["copland_metrics", "copland_environment"]
-READY = lambda x : all(elem in REQUIRED_NODES for elem in x)
+READY = lambda x: all(elem in x for elem in REQUIRED_NODES)
 
 class CoplandMain(Node):
     def __init__(self, __id: str = node_uuid, __random_state: int = 0):
@@ -28,9 +25,22 @@ class CoplandMain(Node):
             SetupArgs, "setup_args", callback=self.callback_setup_args
         )
         self.service_list: List[str] = []
-
+        self.sim_counter = 0
         self.new_sim_publisher_ = self.create_publisher(NewSim, 'new_sim', 2)
-        self.sim_ids: List[str] = []
+        self.end_sim_subscriber = self.create_subscription(EndSim, 'end_sim', self.handle_end_sim, 2)
+
+        self.startup_sync = self.create_timer(1, self.sync_checker)
+
+    def sync_checker(self):
+        self.get_logger().info("Waiting for Nodes...")
+        if READY(self.service_list) == True:
+            self.get_logger().info("Synced, sending dummy end signal.")
+            dummy_msg = EndSim()
+            dummy_msg.ok = True
+            dummy_msg.sim_id = "null"
+            self.handle_end_sim(dummy_msg)
+            self.get_logger().info("Dummy signal published. Destroying sync timer.")
+            self.startup_sync.destroy()
 
     def callback_setup_args(self, __request, __response: SetupArgs):
         self.get_logger().info("Setup Args Request: {}".format(__request.node_name))
@@ -38,36 +48,38 @@ class CoplandMain(Node):
         __response.start_time = int(time.time())
         __response.random_state = self.random_state
         self.service_list.append(__request.node_name)
+        if READY(self.service_list):
+            self.get_logger().info("All Required nodes found, waiting on end_sim falling edge.")
         return __response
 
-    def publish_new_sim(self, __sim_timeout=600):
-        self.get_logger().info("Starting Publish")
-        if not READY(self.service_list):
-            return SimResult.NOTREADY;
-    
+    def publish_new_sim(self, __algorithm_coverage: str, __algorithm_pathing: str, __sim_timeout = 600):
+
+
         msg = NewSim()
-        msg.sim_id = str(uuid.uuid4())
+        # Sim_ID is combination of session id and then an incrementing number
+        msg.sim_id = str(f"{self.id}{self.sim_counter}")
         msg.sim_timeout = __sim_timeout
-        msg.algorithm_specifier = "temp"
+        msg.algorithm_coverage = __algorithm_coverage
+        msg.algorithm_pathing = __algorithm_pathing
+        self.get_logger().info(f"Publishing New Sim. ID: {msg.sim_id}")
+        
+        self.sim_counter += 1
         self.new_sim_publisher_.publish(msg)
-        self.get_logger().info("PUBLISHED! : {}".format(msg.sim_id))
-        return SimResult.END
+        return True
+
+    def handle_end_sim(self, __msg):
+        self.get_logger().info(f"Ending Sim. ID: {__msg.sim_id}, Status: {__msg.ok}")
+        while True:
+            if self.publish_new_sim('interiorscrew', 'astar') == True:
+                break;
+            # self.get_logger().info("Not ready, waiting to create new sim.")
+
 
 def main(args=None):
     rclpy.init()
     copland_main = CoplandMain()
-    while True:
-        rclpy.spin_once(copland_main)
-        sim_result = copland_main.publish_new_sim()
-        time.sleep(5)
-        match sim_result:
-            case SimResult.END:
-                continue;
-                # break;
-            case SimResult.NOTREADY:
-                continue;
-            case _:
-                break;
+    rclpy.spin(copland_main)
+
     rclpy.shutdown()
 
 
--- src/copland_testing/copland_testing/copland_metrics.py
+++ src/copland_testing/copland_testing/copland_metrics.py
@@ -1,5 +1,6 @@
 from rclpy.node import Node
 from coplandnavi_interfaces.srv import SetupArgs
+from coplandnavi_interfaces.msg import NewSim 
 import rclpy
 
 class CoplandMetrics(Node):
@@ -14,6 +15,10 @@ class CoplandMetrics(Node):
         self.setup_req = SetupArgs.Request()
         self.setup_args = self.send_setup_args_request()
         self.get_logger().info('Setup Args Complete.')
+        self.new_sim_subscriber_ = self.create_subscription(NewSim, 'new_sim', self.handle_new_sim, 2)
+
+    def handle_new_sim(self, __msg):
+        self.get_logger().info(f'New Message: {__msg}')
 
     def send_setup_args_request(self):
         # Replace with self node_name attribute if it exists
@@ -29,6 +34,25 @@ Coverage(%)
 Percentage Cleaned(%)
 Seconds Elapsed(int)
 Collision Rate(%)
+"""
+
+"""
+Group into log files: Each log file represents one simulation.
+File name indicates algorithm parameters, i.e. path coverage and pathing algorithms.
+
+astar-interiorscrew-simid.log
+
+---Contents---
+filename: 'astar-interiorscrew-123456.log'
+sim_id: '123456'
+algorithm_pathing: 'astar'
+algorithm_coverage: 'interiorscrew'
+sim_start: unix-timestamp
+sim_end: unix-timestamp
+coverage: 50%
+percentage_cleaned: 50%
+collision_rate: 50%
+
 """
 
 
