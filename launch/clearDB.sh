718511f75dcf28d769a55ceb55f4d8cc807afd48
YammyToast <cyanjamesmail@gmail.com>
2024-05-03 01:15:24 +0000

    test_config launch parameter setup

--- launch/clearDB.sh
+++ launch/clearDB.sh
@@ -1,1 +1,2 @@
+cd ./metrics/
 find . -name '*.json' -delete
--- launch/coplandnavi.py
+++ launch/coplandnavi.py
@@ -1,40 +1,48 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='copland_testing',
-            namespace='coplandnavi',
-            executable='copland_main',
-            name='copland_main'
-        ),
-        Node(
-            package='copland_testing',
-            namespace='coplandnavi',
-            executable='copland_metrics',
-            name='copland_metrics'
-        ),
-        Node(
-            package='copland_testing',
-            namespace='coplandnavi',
-            executable='copland_environment',
-            name='copland_environment'
-        ),
-        Node(
-            package='navi',
-            namespace='coplandnavi',
-            executable='navi_main',
-            name='navi_main'
-
-        ),
-        Node(
-            package='navi',
-            namespace='coplandnavi',
-            executable='navi_navigation',
-            name='navi_navigation'
-        )
-
-    ])
+    test_config = LaunchConfiguration("test_config", default="../test_config.py")
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "test_config",
+                default_value=test_config,
+                description="Python File detailing the set of algorithms and maps used in testing.",
+            ),
+            Node(
+                package="copland_testing",
+                namespace="coplandnavi",
+                executable="copland_main",
+                name="copland_main",
+            ),
+            Node(
+                package="copland_testing",
+                namespace="coplandnavi",
+                executable="copland_metrics",
+                name="copland_metrics",
+            ),
+            Node(
+                package="copland_testing",
+                namespace="coplandnavi",
+                executable="copland_environment",
+                name="copland_environment",
+            ),
+            Node(
+                package="navi",
+                namespace="coplandnavi",
+                executable="navi_main",
+                name="navi_main",
+                parameters=[{"test_config": test_config}],
+            ),
+            Node(
+                package="navi",
+                namespace="coplandnavi",
+                executable="navi_navigation",
+                name="navi_navigation",
+            ),
+        ]
+    )
